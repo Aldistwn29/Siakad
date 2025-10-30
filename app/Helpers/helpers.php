b821ea658e2fb85bd26d71396507f1017bafa1cc
Aldi setiawan <aldi60051@gmail.com>
2025-10-30 23:46:34 +0700

    Feat:Helprs,Enum,Traits

---
+++ app/Helpers/helpers.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\helpers;
+
+use App\Models\AcademicYear;
+
+if(!function_exists('flashMessage')){
+    function flashMessage($message, $type = 'success'){
+        session()->flash('message', $message);
+        session()->flash('type', $type);
+    }
+}
+
+if(!function_exists('signatureMidtrans')){
+    function signatureMidtrans($order_id, $status_code, $gross_amount, $server_key){
+        return hash("sha512", $order_id . $status_code . $gross_amount . $server_key);
+    }
+}
+
+if(!function_exists('activeAcademicYear')){
+    function activeAcademicYear(){
+        AcademicYear::query()->where('is_active', true)->first();
+    }
+}
+
+if(!function_exists('getLetterGrade')){
+    function getLetterGrade($grade){
+        return match(true){
+            $grade >= 90 => 'A',
+            $grade >= 85 => 'A-',
+            $grade >= 80 => 'B+',
+            $grade >= 75 => 'B',
+            $grade >= 70 => 'B-',
+            $grade >= 65 => 'C+',
+            $grade >= 60 => 'C',
+            $grade >= 55 => 'C-',
+            $grade >= 50 => 'C',
+
+            default => 'E',
+        };
+    }
+}
