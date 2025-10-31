2daf26e982e0d94191af8843212b134853672c4d
Aldi setiawan <aldi60051@gmail.com>
2025-11-01 01:37:35 +0700

    Feact:menyesuaikan dan menambah komponen

---
+++ resources/js/Components/AlertAction.jsx
@@ -0,0 +1,25 @@
+import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTrigger } from "@/Components/ui/alert-dialog";
+import { AlertDialogTitle } from "@radix-ui/react-alert-dialog";
+
+export default function AlertAction({
+    trigger,
+    action,
+    title = 'Apakah anda benar-benar yakin',
+    description = 'Tindakan ini tidak dapat dibatalkan. Tindakan ini akan menghapus data secara permanen dan menghapus data anda dari server kami.',
+}) {
+    return (
+        <AlertDialog>
+            <AlertDialogTrigger asChild>{trigger}</AlertDialogTrigger>
+            <AlertDialogContent>
+                <AlertDialogHeader>
+                    <AlertDialogTitle>{title}</AlertDialogTitle>
+                    <AlertDialogDescription>{description}</AlertDialogDescription>
+                </AlertDialogHeader>
+                <AlertDialogFooter>
+                    <AlertDialogCancel>Cancel</AlertDialogCancel>
+                    <AlertDialogAction onClick={action}>Continue</AlertDialogAction>
+                </AlertDialogFooter>
+            </AlertDialogContent>
+        </AlertDialog>
+    )
+}
--- resources/js/Components/InputError.jsx
+++ resources/js/Components/InputError.jsx
@@ -1,8 +1,10 @@
+import { cn } from "@/lib/utils";
+
 export default function InputError({ message, className = '', ...props }) {
     return message ? (
         <p
             {...props}
-            className={'text-sm text-red-600 ' + className}
+            className={cn('text-xs text-red-500', className)}
         >
             {message}
         </p>
